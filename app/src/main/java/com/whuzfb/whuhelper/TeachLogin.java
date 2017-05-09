0c6ffa0127513ff58e3f967a30a76ec1c1c640c3
zfb <zfb132@gmail.com>
2017-05-09 20:34:06 +0800

    项目初始化

---
+++ app/src/main/java/com/whuzfb/whuhelper/TeachLogin.java
@@ -0,0 +1,17 @@
+package com.whuzfb.whuhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by zfb15 on 2017/5/9.
+ */
+
+public class TeachLogin extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teachlogin);
+    }
+}
