bed299a4665508c9f904918f8e6d2b11e60736a4
zfb <zfb132@gmail.com>
2018-09-03 01:09:00 +0800

    修复因Activity到Fragment而出现的问题

--- app/src/main/java/sign/AllSign.java
+++ app/src/main/java/sign/AllSign.java
@@ -1,31 +1,26 @@
 package sign;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.whuzfb.whuhelper.MainActivity;
 import com.whuzfb.whuhelper.R;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,31 +29,50 @@ import java.util.Map;
  * Created by zfb15 on 2017/10/8.
  */
 
-public class AllSign extends Activity {
+public class AllSign extends Fragment {
     //控件
     private TextInputEditText et_url=null;
     private TextInputEditText et_param=null;
+    private TextInputEditText et_user=null;
+    private TextInputEditText et_pwd=null;
     private Button btn_up=null;
 
     private Map<String, String> headers=new HashMap<>();
     public static String url="";
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_allsign);
-        initView();
-        setListener();
-        //不自动弹出软键盘
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    private Context context=null;
+    private FragmentActivity fragmentActivity=null;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         url=WebURL.APIHOST_NETEASEMUSIC;
+        // 获取上下文用于其他函数
+        context=getActivity();
+        fragmentActivity=getActivity();
     }
 
-    private void initView(){
-        et_url=(TextInputEditText)findViewById(R.id.et_url);
-        et_param=(TextInputEditText)findViewById(R.id.et_param);
-        btn_up=(Button)findViewById(R.id.btn_up);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_allsign, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        setListener();
+        //不自动弹出软键盘
+        fragmentActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+    private void initView(View v){
+        et_url=(TextInputEditText)v.findViewById(R.id.et_url);
+        et_param=(TextInputEditText)v.findViewById(R.id.et_param);
+        et_user=(TextInputEditText)v.findViewById(R.id.et_allsign_user);
+        et_pwd=(TextInputEditText)v.findViewById(R.id.et_allsign_pwd);
+        btn_up=(Button)v.findViewById(R.id.btn_up);
     }
 
     private void setListener(){
@@ -74,8 +88,8 @@ public class AllSign extends Activity {
     public Map<String,String> setParams(){
         //获取，cooking和表单属性，下面map存放post时的数据
         Map<String, String> post_params=new HashMap<>();
-        post_params.put("phone","13297987851");
-        post_params.put("password", "1529017133");
+        post_params.put("phone",et_user.getText().toString());
+        post_params.put("password", et_pwd.getText().toString());
         return post_params;
     }
 
@@ -104,7 +118,7 @@ public class AllSign extends Activity {
 
     //Toast显示字符串
     public void showError(String str) {
-        Toast toast = Toast.makeText(this, str, Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(fragmentActivity, str, Toast.LENGTH_LONG);
         toast.setGravity(Gravity.BOTTOM, 0, 200);
         toast.show();
     }
